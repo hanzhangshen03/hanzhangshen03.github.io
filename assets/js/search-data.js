5accadb2c975d87b5a73dce2b6ec151f2a880050
hanzhangshen03 <shenhanzhang@outlook.com>
2025-07-22 22:53:38 +0000

    Deploying to gh-pages from @ hanzhangshen03/hanzhangshen03.github.io@aa26701d8e368e0812e3f4102554427eeb54737a 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,78 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-i-joined-encode-lab-at-westlake-university-today-as-a-visiting-student-advised-by-huan",
+          title: 'I joined ENCODE Lab at Westlake University today as a visiting student, advised...',
+          description: "",
+          section: "News",},{id: "news-awarded-the-adrian-knight-prize-in-computer-science-by-robinson-college-university-of-cambridge",
+          title: 'Awarded the Adrian Knight Prize in Computer Science by Robinson College, University of...',
+          description: "",
+          section: "News",},{id: "news-elected-as-a-junior-scholar-of-robinson-college-university-of-cambridge",
+          title: 'Elected as a Junior Scholar of Robinson College, University of Cambridge.',
+          description: "",
+          section: "News",},{id: "news-i-am-joining-similarity-detection-ai-team-at-meta-london-today-as-a-software-engineer-intern",
+          title: 'I am joining Similarity Detection AI team at Meta London today as a...',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%73%68%65%6E%68%61%6E%7A%68%61%6E%67@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/hanzhangshen03", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/hanzhang-shen-747629224", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
